d81378753bf10e1d14938877726713c7554abeb6
Maja <majam1616@gmail.com>
2022-04-10 00:50:44 -0400

    removed commented lines

--- gmail_spam/constants.py
+++ gmail_spam/constants.py
@@ -4,7 +4,3 @@ import os
 # root of the whole project
 PROJECT_ROOT=os.path.abspath(PurePath(__file__).parents[1])
 LOG_DIR = os.path.join(PROJECT_ROOT, 'logs')
-
-# print(PROJECT_ROOT)
-# print(LOG_DIR)
-# print(__file__)
